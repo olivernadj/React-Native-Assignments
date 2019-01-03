c0cdfccccb27e272f0aa0858ab04f47df1c2ed80
oliver nadj <mr.oliver.nadj@gmail.com>
2019-01-03 20:40:47 +0700

    Basic movie listing has done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Container, Row, Col } from 'reactstrap';
 import './App.css';
+import Item from "./components/Item/Item";
 
 class App extends Component {
+
+  state = {
+    listMovies: []
+  }
+
+  async componentDidMount () {
+    const response = await  fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed');
+    const movieData = await response.json();
+    this.setState({listMovies: movieData.results})
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Row>
+          <Col>Header</Col>
+        </Row>
+        <Row>
+          {this.state.listMovies.map((m) => {
+            return (
+              <Col xs="6">
+                <Item
+                  poster_path={m.poster_path}
+                  title={m.title}
+                  release_date={m.release_date}
+                  overview={m.overview}
+                />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     );
   }
 }
---
+++ src/components/Item/Item.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+const Item = (props) => {
+  // console.log('Item.js props', props);
+  return (
+    <div>
+      <Card>
+        <CardImg top width="100%" src={'https://image.tmdb.org/t/p/w185_and_h278_bestv2/' + props.poster_path}
+                 alt="Card image cap"/>
+        <CardBody>
+          <CardTitle>{props.title}</CardTitle>
+          <CardSubtitle>{props.release_date}</CardSubtitle>
+          <CardText>{props.overview}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Item;
