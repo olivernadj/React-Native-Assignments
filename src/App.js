0a2bb12e8c24b1fb723c40c823413c7afec12dc4
oliver nadj <mr.oliver.nadj@gmail.com>
2019-01-03 20:58:23 +0700

    Minimum requirements for homework is done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactLoading from "react-loading";
 import { Container, Row, Col } from 'reactstrap';
 import './App.css';
 import Item from "./components/Item/Item";
@@ -15,26 +16,39 @@ class App extends Component {
     this.setState({listMovies: movieData.results})
   }
 
-
   render() {
+
+    let listMovies = null;
+
+    if (this.state.listMovies.length === 0) {
+      listMovies = (
+        <Col sm="12" md={{ size: 6, offset: 3 }}>
+          <ReactLoading type="bubbles" color="#ooo" />
+        </Col>
+      );
+    } else {
+      listMovies = this.state.listMovies.map((m) => {
+        return (
+          <Col xs="6" key={m.id}>
+            <Item
+              poster_path={m.poster_path}
+              title={m.title}
+              release_date={m.release_date}
+              overview={m.overview}
+            />
+          </Col>
+        );
+      });
+    }
+
+
     return (
       <Container>
         <Row>
           <Col>Header</Col>
         </Row>
         <Row>
-          {this.state.listMovies.map((m) => {
-            return (
-              <Col xs="6">
-                <Item
-                  poster_path={m.poster_path}
-                  title={m.title}
-                  release_date={m.release_date}
-                  overview={m.overview}
-                />
-              </Col>
-            );
-          })}
+          {listMovies}
         </Row>
       </Container>
     );
