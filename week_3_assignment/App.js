ad835e87338928f4903a1faa138cac6b8c4472e6
oliver nadj <oliver.nadj@lazada.com>
2019-01-24 20:17:07 +0700

    Added very basic requironments

--- week_3_assignment/App.js
+++ week_3_assignment/App.js
@@ -1,41 +1,179 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList } from 'react-native';
+import {
+  List,
+  ListItem
+} from "react-native-elements";
 import firedb from './database.js';
 
 export default class App extends React.Component {
 
-  componentDidMount() {
-    firedb.ref('/messages').on('value', (snapshot) => {
-      console.log(snapshot.val());
-    });
+  state = {
+    nickname: '',
+    message: '',
+    entered: false,
+    loading: false,
+    chat: []
+  };
 
-    firedb.ref('/messages').push({
-      name: "Pushmethod",
-      message: "I am not sure will it work.",
-      timestamp: Date.now()
+  componentDidMount() {
+    firedb.ref('messages').limitToLast(20).on('value', (snapshot) => {
+      const res = snapshot.val();
+      const chat = [];
+      for (let key in snapshot.val()) {
+        chat.push(
+          {...res[key], id: key}
+        );
+      }
+      this.setState({chat: chat});
+      // console.log(snapshot.val());
     });
 
   }
 
-  render() {
+  enterToChatHandler = () => {
+    //console.log(this.state.nickname);
+    this.setState({entered: true});
+  };
+
+
+  sendMessageHandler = () => {
+    console.log('sendMessageHandler');
+    console.log(this.state.message);
+    firedb.ref('messages').push({
+      name: this.state.nickname,
+      message: this.state.message,
+      timestamp: Date.now()
+    });
+    this.setState({
+      message : ''
+    })
+  };
+
+  onChangeMessage = (text ) => {
+    this.setState(({
+      message : text
+    }))
+  }
+
+  renderSeparator = () => {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-        />
-      </View>
+      <View
+        style={{
+          height: 1,
+          width: "86%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "14%"
+        }}
+      />
     );
+  };
+
+
+  render() {
+
+    let layout = '';
+
+    if (this.state.entered) {
+      layout = (
+        <View style={styles.container}>
+          <View style={styles.inputInfo}>
+            <FlatList
+              data={this.state.chat}
+              refreshing={this.state.loading}
+              renderItem={({item}) => (
+                <View>
+                  <Text>
+                    {JSON.stringify(item)}
+                  </Text>
+                </View>)}
+              keyExtractor={(item) => item.id.toString()}
+              ItemSeparatorComponent={this.renderSeparator}
+              //ListFooterComponent={this.renderFooter}
+            />
+          </View>
+          <View style={styles.inputForm}>
+            <View style={styles.inputText}>
+              <TextInput
+                style={{height: 35, borderColor: 'gray', borderWidth: 1, margin: 5, backgroundColor: '#FFF'}}
+                onChangeText={this.onChangeMessage}
+                value={this.state.message}
+              />
+            </View>
+            <View style={styles.inputButton}>
+              <Button
+                onPress={this.sendMessageHandler}
+                title="Send"
+                accessibilityLabel="Send message"
+              />
+            </View>
+          </View>
+        </View>
+      );
+    } else {
+      layout = (
+        <View style={styles.container}>
+          <View style={styles.inputInfo}>
+            <Text>Please input your nickname for chat!</Text>
+          </View>
+          <View style={styles.inputForm}>
+            <View style={styles.inputText}>
+              <TextInput
+                style={{height: 35, borderColor: 'gray', borderWidth: 1, margin: 5, backgroundColor: '#FFF'}}
+                onChangeText={(v) => this.setState({nickname:v})}
+                value={this.state.nickname}
+              />
+            </View>
+            <View style={styles.inputButton}>
+              <Button
+                onPress={this.enterToChatHandler}
+                title="OK"
+                accessibilityLabel="Enter to chat"
+              />
+            </View>
+          </View>
+        </View>
+      );
+    }
+
+    return layout;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 30,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ccc',
+  },
+  inputInfo: {
+    margin: 5,
+    // borderColor: 'gray',
+    // borderWidth: 1,
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: '#ccc'
+  },
+  inputForm: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#ccc'
+  },
+  inputText: {
+    flex: 4,
+    backgroundColor: '#ccc'
+  },
+  inputButton: {
+    flex: 1,
+    backgroundColor: '#ccc',
+    padding: 5,
   },
 });
--- week_3_assignment/README.md
+++ week_3_assignment/README.md
@@ -1,6 +1,6 @@
 # Project 3 - *week_3_assignment*
 
-Time spent: **X** hours spent in total
+Time spent: 6 hours spent in total
 
 ## User Stories
 
